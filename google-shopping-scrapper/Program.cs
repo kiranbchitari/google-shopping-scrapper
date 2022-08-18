ac5d8494465096735686d6c4bc9c07cee61c3bfe
kiranbchitari <36378061+kiranbchitari@users.noreply.github.com>
2022-08-18 19:51:04 +0530

    Update Program.cs

--- google-shopping-scrapper/Program.cs
+++ google-shopping-scrapper/Program.cs
@@ -54,9 +54,10 @@ namespace google_shopping_scrapper
                             {
                                 var name = doc3.DocumentNode.SelectNodes("(//span[@class='BvQan sh-t__title-pdp sh-t__title translate-content'])").FirstOrDefault();
                                 product.name = name.InnerHtml.ToString();
-                                var imageUrl = doc3.DocumentNode.SelectNodes("(//div[@class='Xkiaqc sm3F0e'])").FirstOrDefault();
-                                string imagesrc = imageUrl.InnerHtml.ToString();
-                                product.imageUrl = Regex.Match(imagesrc, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
+                                var imageUrl = doc3.DocumentNode.Descendants("img")
+                                               .Select(e => e.GetAttributeValue("src", null))
+                                               .Where(s => !String.IsNullOrEmpty(s) && s.Contains("com/shopping?")).ToList().FirstOrDefault();
+                                product.ImageUrl = imageUrl;
                                 try
                                 {
                                     var des = doc3.DocumentNode.SelectNodes("(//span[@class='sh-ds__full-txt translate-content'])").FirstOrDefault();
